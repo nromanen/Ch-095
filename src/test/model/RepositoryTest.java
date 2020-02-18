5450a1154da6b1069bb5f9e936a2957c6604ca82
Peter <petrofediuk004@gmail.com>
2020-02-18 15:15:17 +0200

    Changed RepositoryTest class

--- src/test/model/RepositoryTest.java
+++ src/test/model/RepositoryTest.java
@@ -1,75 +1,61 @@
 package model;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Stream;
-
 
 public class RepositoryTest {
-    private static Set<Student> studentSet = new HashSet<>();
-    private  Student student = new Student("aa", "aa", 2.5);
+    private Set<Student> studentSet = new HashSet<>();
+    private Student student = new Student("aa", "aa", 2.5);
+    private Repository repository = new Repository(studentSet);
 
-    static Stream<Repository> methodToTest() {
-        return Stream.of(new Repository(studentSet));
+    @Before
+    public void setUp() {
+        repository.populateStudents();
     }
 
-    @ParameterizedTest
-    @MethodSource("methodToTest")
-    public void populateStudentTest(Repository repository) {
-        int studentRepoSizeBefore = repository.getStudents().size();
-        repository.populateStudents();
-        int studentRepoSizeAfter = repository.getStudents().size();
-        assertTrue(studentRepoSizeAfter > studentRepoSizeBefore);
+    @Test
+    public void populateStudentTest() {
+        int actualSize = repository.getStudents().size();
+        assertEquals("Size of repository must be 2", actualSize, 2);
     }
 
-    @ParameterizedTest
-    @MethodSource("methodToTest")
-    public void removeStudentTest(Repository repository) {
-        repository.populateStudents();
+    @Test
+    public void removeStudentTest() {
         Student firstStudent = new Student("aa", "aa", 2.5);
         boolean firstFlag = repository.removeStudent(firstStudent);
         assertTrue(firstFlag);
-        Student secondStudent = new Student("aa", "aa", 12);
-        boolean secondFlag = repository.removeStudent(secondStudent);
-        assertFalse(secondFlag);
     }
 
-    @ParameterizedTest
-    @MethodSource("methodToTest")
-    public void findByNameTest(Repository repository) {
-        repository.populateStudents();
+    @Test
+    public void findByNameTest() {
         String studentName = "aa";
         Set<Student> studentSet = repository.findByName(studentName);
         assertNotNull(studentSet);
-        assertEquals(studentSet.size(), 1);
+        assertEquals("Size of repository must be 2", studentSet.size(), 1);
     }
 
-    @ParameterizedTest
-    @MethodSource("methodToTest")
-    public void removeByRankTest(Repository repository) {
-        repository.populateStudents();
+    @Test
+    public void removeByRankTest() {
         double rank = 2.5;
         repository.removeByRank(rank);
-        assertEquals(repository.getStudents().size(), 2);
+        assertEquals(repository.getStudents().size(), 1);
     }
 
-    @ParameterizedTest
-    @MethodSource("methodToTest")
-    public void addStudentTest(Repository repository) {
+    @Test
+    public void addStudentTest() {
         boolean firstFlag = repository.addStudent(student);
         assertTrue(firstFlag);
         boolean secondFlag = repository.add(student);
         assertFalse(secondFlag);
     }
 
-    @ParameterizedTest
-    @MethodSource("methodToTest")
-    public void setStudentTest(Repository repository) {
+    @Test
+    public void setStudentTest() {
         Set<Student> studentSet = new HashSet<>();
         studentSet.add(student);
         repository.setStudents(studentSet);
